b3b31bc49786766170e3068d3cd445147003008f
Dev <devkumarpatel20@gnu.ac.in>
2022-08-07 14:57:53 +0530

    Ex1

---
+++ src/main/kotlin/Ex1.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val a: Int = 22
+    val b: Float = 1.5f
+    val c: Char = 'T'
+    val d: String = "DRP"
+    val e: Boolean = false
+    val g: Double = 96.36
+    val h: Long = 338927847L
+    val i: Short = -2
+    val j: Byte = 127
+
+    println("Integer Value $a")
+    println("Float Value $b")
+    println("Character Value $c")
+    println("String Value $d")
+    println("Boolean Value $e")
+    println("Double Value $g")
+    println("Long Value $h")
+    println("Short Value $i")
+    println("Byte Value $j")
+}
