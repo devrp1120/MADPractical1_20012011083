ab683ace6019ef79bd0b5efa51b26ba77a017b5f
Dev <devkumarpatel20@gnu.ac.in>
2022-08-09 14:30:01 +0530

    Ex7,9

---
+++ src/main/kotlin/Ex7.kt
@@ -0,0 +1,21 @@
+fun main() {
+    print("Enter a number you want it's factorial: ")
+    var n = readLine()!!.toInt()
+    println("Factorial using Recursion " + recFact(n))
+    println("Factorial using tailrec " + fact(n))
+}
+
+fun recFact(n: Int): Int {
+    if (n == 1 || n == 0) {
+        return 1
+    }
+    return n * recFact(n - 1)
+}
+
+tailrec fun fact(n: Int, temp: Int = 1): Int {
+    return if (n == 1) {
+        temp
+    } else {
+        fact(n - 1, temp * n)
+    }
+}
