a53205b0a9f376bbbffbf25913f2add2f2ab3bc5
Dev <devkumarpatel20@gnu.ac.in>
2022-08-12 11:17:37 +0530

    Ex7,9

--- src/main/kotlin/Ex7.kt
+++ src/main/kotlin/Ex7.kt
@@ -1,15 +1,15 @@
 fun main() {
     print("Enter a number you want it's factorial: ")
     val n = readLine()!!.toInt()
-    println("Factorial using Recursion " + recFact(n))
+    println("Factorial using Recursion " + recursionFact(n))
     println("Factorial using tailrec " + fact(n))
 }
 
-fun recFact(n: Int): Int {
+fun recursionFact(n: Int): Int {
     if (n == 1 || n == 0) {
         return 1
     }
-    return n * recFact(n - 1)
+    return n * recursionFact(n - 1)
 }
 
 tailrec fun fact(n: Int, temp: Int = 1): Int {
