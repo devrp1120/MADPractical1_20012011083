e4050106874d0d47f53cdc425e35f3284f871cb3
Dev <devkumarpatel20@gnu.ac.in>
2022-08-18 10:01:21 +0530

    ex8

--- src/main/kotlin/Ex10.kt
+++ src/main/kotlin/Ex10.kt
@@ -3,7 +3,7 @@ class Car {
     var originalPrice: Int = 3000000
     var owner: String = "Dev"
     var mileDrive: Int = 4000
-    var currentPrice: Int = 2850000
+    var curentPrice: Int = 2850000
 
 
 }
