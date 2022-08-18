b6ef0d64c110609645cf596f3b04db0900b8506e
Dev <devkumarpatel20@gnu.ac.in>
2022-08-18 09:59:35 +0530

    ex8

---
+++ src/main/kotlin/Ex10.kt
@@ -0,0 +1,9 @@
+class Car {
+    var model: String = "Audi A6, 2018"
+    var originalPrice: Int = 3000000
+    var owner: String = "Dev"
+    var mileDrive: Int = 4000
+    var currentPrice: Int = 2850000
+
+
+}
